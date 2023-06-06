cc19655a6a3bd7a3f1f1acb781675f35b73af25a
Anatoli Pavlov <18app81@gmail.com>
2023-06-06 18:04:52 +0300

    Create Cube model

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://127.0.0.1:27017/rubik-cube';
+
+async function dbConnect() {
+    await mongoose.connect(connectionString);
+}
+
+module.exports = dbConnect;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
+const dbConnect = require('./config/database');
 const routes = require('./routes');
+
 const app = express();
+
+require('./config/handlebars')(app);
 
 app.use('/static', express.static('public'));
 
 // всяка POST заявка минава през "urlencoded", което е "middleware" на "express" и парсва данните в обект, който достъпваме в "req.body" 
 app.use(express.urlencoded({ extended: false }));
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', './src/views');
-
 // ПРИ ВСИЧКИ ЗАЯВКИ ИЗПОЛЗВАЙ ТОЗИ РУТЕР
 app.use(routes);
 
-app.listen(5000, () => console.log(`App is listening on port 5000`));
+dbConnect()
+    .then(() => {
+        console.log('Database is connected successfuly');
+        app.listen(5000, () => console.log(`App is listening on port 5000`));
+
+    })
+    .catch(err => console.log('Database error: ', err.message));
+
+
